e97bbc0151fd11861413b3f4ba49c9fc63139ac5
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-10 23:10:10 +0000

    Automatically backed up by Learn

--- db/migrate/20200510230739_create_dogs.rb
+++ db/migrate/20200510230739_create_dogs.rb
@@ -4,4 +4,14 @@ class CreateDogs < ActiveRecord::Migration[5.2]
   #   t.string :name
   #   t.string :breed
   #  end
+  def up
+  create_table :dogs do |t|
+    t.string :name
+    t.string :breed
+  end
+end
+
+def down
+  drop_table :dogs
+end
 end
